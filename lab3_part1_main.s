d76c66765364bd4a2fce6abbb0883fd877015379
vsbenas <vsbenas@gmail.com>
2017-11-14 15:56:01 -0500

    initial files

---
+++ lab3_part1_main.s
@@ -0,0 +1,13 @@
+# Print ten in octal, hexadecimal, and decimal
+# Use the following C functions:
+#     printHex ( int ) ;
+#     printOct ( int ) ;
+#     printDec ( int ) ;
+
+.global main
+
+main:
+# ...
+
+  ret	# Make sure this returns to main's caller
+
---
+++ lab3_printn.s
@@ -0,0 +1,19 @@
+/*********
+ * 
+ * Write the assembly function:
+ *     printn ( char * , ... ) ;
+ * Use the following C functions:
+ *     printHex ( int ) ;
+ *     printOct ( int ) ;
+ *     printDec ( int ) ;
+ * 
+ * Note that 'a' is a valid integer, so movi r2, 'a' is valid, and you don't need to look up ASCII values.
+ *********/
+
+.global	printn
+printn:
+# ...
+
+    ret
+
+
