6b29a84c0e4c144e12f707220413dafcfeedf355
LAPTOP-0BM0R1NJ\BaiJiaxin <jbai0318@gmail.com>
2017-12-04 18:59:40 -0500

    Auto stash before merge of "master" and "origin/master"

--- board.c
+++ board.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 /*
 1. We are using 7 type of blocks, they are numbered in sequence
 2. 2 implies it is the center, 1 means the one should change during the rotation
@@ -80,5 +81,75 @@ void createNewBlock(int* board_addr, int* gamestate) {
   // if cannot add a new block, gameover
   if (result == -1) {
     setLose(gamestate);
+  }
+}
+
+void copy_board(int* new_board, int* board_addr) {
+  int i;
+  for (i=0; i<240; i++) {
+    new_board[i] = board_addr[i];
+  }
+}
+
+void remove_falling_block(int* board_addr) {
+  int i;
+  for (i = 0; i<240; i++) {
+    if (board_addr[i] > 1) board_addr[i] = 0;
+  }
+}
+
+/* cell types:
+0 - empty block
+1 - taken block
+2 - falling figure
+3 - center of the figure
+*/
+
+void move_block(int* board_addr, int direction) {
+  // 0   1  2   3
+  // left up down right
+
+  int temp_board[240]={0};
+  int i;
+  int valid = 1;
+  int x_min = 10;
+  int x_max = -1;
+  // check x the min and max of block
+  for (int i=0; i<240; i++) {
+    if ( *(board_addr+i) > 1 && *(board_addr+i) < 4) {
+      int x = i%10;
+      // update the min and max
+      if (x_min > x) x_min = x;
+      if (x_max < x) x_max = x;
+
+      // if on the boundary and coresbonding move is invalid
+      if (x_min == 0 && direction == 1) return;
+      if (x_max == 9 && direction == 3) return;
+    }
+  }
+
+  // collision checking
+  for (int i=0; i<240; i++) {
+    if ( *(board_addr+i) > 1 && *(board_addr+i) < 4) {
+      if (direction == 0) {
+        if ( *(board_addr+i-1)<2) return;
+      } else if (direction == 3) {
+        if ( *(board_addr+i+1)<2) return;
+      }
+    }
+  }
+
+  // things are valid, move the block
+  copy_board(temp_board, board_addr);
+  remove_falling_block(temp_board);
+
+  for (int i=0; i<240; i++) {
+    if ( *(board_addr+i) > 1 && *(board_addr+i) < 4) {
+      if (direction == 0) {
+        temp_board[i-1] = board_addr[i];
+      } else if (direction == 3) {
+        temp_board[i+1] = board_addr[i];
+      }
+    }
   }
 }
